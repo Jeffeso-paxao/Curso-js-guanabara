d990c058fe00e46f57a556dafd66665f20c84d90
Jefferson Lima <jeffersonlima94@hotmail.com>
2019-09-13 18:55:52 -0300

    Condicionais

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var velocidade = 12;
+console.log(`A velocidade do seu carro é ${velocidade} km/h`);
+//condição simples
+if (velocidade > 60) {
+  console.log(`Você ultrapassou a velocidade. MULTADO!`);
+}
+console.log(`Dirija sempre usando cinto de segurança!`);
